78c7d42841948c31725866bc35ba357e549119f0
Helen Hood <helen.c.hood@gmail.com>
2014-08-25 12:55:46 -0400

    Indentation & whitespace edits

--- game_time.rb
+++ game_time.rb
@@ -4,16 +4,16 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 winners = []
-CSV.foreach('teams.csv',headers:true,header_converters: :symbol) do |row|
- if row[:home_score].to_i > row[:away_score].to_i
-  winners << row[:home_team]
-elsif row[:away_score].to_i > row[:home_score].to_i
-  winners << row[:away_team]
-end
+CSV.foreach('teams.csv', headers:true, header_converters: :symbol) do |row|
+  if row[:home_score].to_i > row[:away_score].to_i
+    winners << row[:home_team]
+  elsif row[:away_score].to_i > row[:home_score].to_i
+    winners << row[:away_team]
+  end
 end
 
 losers = []
-CSV.foreach('teams.csv',headers:true,header_converters: :symbol) do |row|
+CSV.foreach('teams.csv', headers:true, header_converters: :symbol) do |row|
   if row[:home_score].to_i < row[:away_score].to_i
     losers << row[:home_team]
   elsif row[:away_score].to_i < row[:home_score].to_i
@@ -21,35 +21,31 @@ CSV.foreach('teams.csv',headers:true,header_converters: :symbol) do |row|
   end
 end
 
-
 win_count = Hash.new 0
-  winners.each do |row|
-    win_count[row] += 1
-  end
+winners.each do |row|
+  win_count[row] += 1
+end
 
 loss_count = Hash.new 0
-  losers.each do |row|
-    loss_count[row] += 1
-  end
-
+losers.each do |row|
+  loss_count[row] += 1
+end
 
 def find_teams
   teams = []
-  CSV.foreach('teams.csv',headers:true,header_converters: :symbol) do |row|
+  CSV.foreach('teams.csv', headers:true, header_converters: :symbol) do |row|
     teams << row[:home_team]
     teams << row[:away_team]
   end
   teams.uniq
 end
 
-
 ##======ROUTES===========#######
-
 
 get '/leaderboard' do
   @team = find_teams
   @wins = win_count
   @losses = loss_count
-erb :leaders
-end
 
+  erb :leaders
+end
